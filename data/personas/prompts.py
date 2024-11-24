a1915d36068144e33acb148f73850ae9cf9b5b6e
Anthony Susevski <asusevski@gmail.com>
2024-11-23 21:23:23 -0500

    add prompts v1

---
+++ data/personas/prompts.py
@@ -0,0 +1,35 @@
+PERSONA_CHARACTERISTICS = [
+    "Curious and Analytical",
+    "Goal-Oriented",
+    "Adaptable Learner",
+    "Perfectionist with Practicality",
+    "Innovative and Strategic",
+    "Structured Planner",
+    "Team-Oriented",
+    "Continuous Improver",
+    "Efficiency-Driven",
+    "Tech-Savvy",
+    "Detail-Oriented Developer",
+    "Full-Stack Perspective",
+    "DevOps Mindset",
+    "Innovator",
+    "Explorer",
+    "Mentor and Collaborator",
+    "Visionary"
+]
+# expects that the MPP has at least one bill or motion
+NO_SHOT_DEFAULT_PROMPT_LONG_FORM = """Bills: {bills}
+Motions: {motions}
+
+Based on the above bills and motions brought forth by a politician, write a couple sentences maximum \
+to describe the personality and backstory of the politician. Only return the persona of the politician."""
+
+NO_SHOT_DEFAULT_PROMPT_SHORT_FORM = """Bills: {bills}
+Motions: {motions}
+
+Characteristics: {characteristics}
+Based on the above bills and motions brought forth by a politician, describe the politician in no more\
+than three of the above persona characteristics."""
+
+NO_SHOT_PROMPT_NO_MOTIONS = ...
+NO_SHOT_PROMPT_NO_BILLS = ...
